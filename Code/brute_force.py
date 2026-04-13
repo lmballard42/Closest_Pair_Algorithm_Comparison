0efc49a5d5ee792a5764250c1f175ffbfb7874c0
snfrisbee42 <snfrisbee42@tntech.edu>
2026-04-13 13:52:18 -0500

    complete the closest_pair function

--- Code/brute_force.py
+++ Code/brute_force.py
@@ -3,4 +3,21 @@ import math
 def distance(x, y):
     return math.sqrt((y[0] - x[0])**2 + (y[1] - x[1])**2)
 
-def closest_pair():
+def closest_pair(points):
+    n = len(points)
+
+    if n < 2:
+        return None
+    
+    min_dist = float('inf')
+    closest_points = None
+
+    for i in range (n-1):
+        for j in range(i + 1, n):
+            dist = distance(points[i], points[j])
+
+            if dist < min_dist:
+                min_dist = dist
+                closest_points = (points[i], points[j])
+    
+    return min_dist, closest_points
