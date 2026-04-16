41f6301702402e377897e972782598a3e9c705a2
snfrisbee42 <snfrisbee42@tntech.edu>
2026-04-15 19:49:07 -0500

    Add comments to brute_force.py

--- Code/brute_force.py
+++ Code/brute_force.py
@@ -7,22 +7,26 @@ def distance(x, y):
 def closest_pair(points):
     n = len(points)
 
+    # If there is not enough points
     if n < 2:
         return None
     
     min_dist = float('inf')
     closest_points = None
 
+    # Loop to compare each point
     for i in range (n-1):
         for j in range(i + 1, n):
             dist = distance(points[i], points[j])
 
+            # To see if the distance is smaller than the current minimum distance
             if dist < min_dist:
                 min_dist = dist
                 closest_points = (points[i], points[j])
     
     return min_dist, closest_points
 
+# Timer to see how long the Brute Algorithm takes
 start = time.perf_counter()
 
 result = closest_pair()
